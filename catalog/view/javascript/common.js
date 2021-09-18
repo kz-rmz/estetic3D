f49eb627abb7cf96365058a313ee6f172c8593fd
kz-rmz <magazumov@gmail.com>
2021-09-18 19:03:16 +0600

    common.js file tooltip bug fix

--- catalog/view/javascript/common.js
+++ catalog/view/javascript/common.js
@@ -127,13 +127,13 @@ $(document).ready(function() {
 	});
 
 	// tooltips on hover
-	$('[data-toggle=\'tooltip\']').tooltip({container: 'body'});
+// 	$('[data-toggle=\'tooltip\']').tooltip({container: 'body'});
 
-	// Makes tooltips work on ajax generated content
-	$(document).ajaxStop(function() {
-		$('[data-toggle=\'tooltip\']').tooltip({container: 'body'});
-	});
-});
+// 	// Makes tooltips work on ajax generated content
+// 	$(document).ajaxStop(function() {
+// 		$('[data-toggle=\'tooltip\']').tooltip({container: 'body'});
+// 	});
+// });
 
 // Cart add remove functions
 var cart = {
