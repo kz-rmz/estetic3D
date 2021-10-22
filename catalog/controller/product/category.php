34c289f150ed6aca2cfaa5a7e6a5497d2b4a0476
kz-rmz <magazumov@gmail.com>
2021-10-22 17:20:27 +0600

    Added category list inside category page

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -136,12 +136,19 @@ class ControllerProductCategory extends Controller {
 			$results = $this->model_catalog_category->getCategories($category_id);
 
 			foreach ($results as $result) {
+				if ($result['image']) {
+					$result['image'] = $this->model_tool_image->resize($result['image'], '1200', '800');
+				} else {
+					$result['image'] = '';
+				}
+
 				$filter_data = array(
 					'filter_category_id'  => $result['category_id'],
-					'filter_sub_category' => true
+					'filter_sub_category' => true,
 				);
 
 				$data['categories'][] = array(
+					'image' => $result['image'],
 					'name' => $result['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
 					'href' => $this->url->link('product/category', 'path=' . $this->request->get['path'] . '_' . $result['category_id'] . $url)
 				);
@@ -333,7 +340,6 @@ class ControllerProductCategory extends Controller {
 
 			$data['results'] = sprintf($this->language->get('text_pagination'), ($product_total) ? (($page - 1) * $limit) + 1 : 0, ((($page - 1) * $limit) > ($product_total - $limit)) ? $product_total : ((($page - 1) * $limit) + $limit), $product_total, ceil($product_total / $limit));
 
-			// http://googlewebmastercentral.blogspot.com/2011/09/pagination-with-relnext-and-relprev.html
 			if ($page == 1) {
 			    $this->document->addLink($this->url->link('product/category', 'path=' . $category_info['category_id']), 'canonical');
 			} else {
